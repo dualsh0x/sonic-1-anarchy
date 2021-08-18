af5770833693aef77bc6763f58f2dae69587712c
dualsh0x <85138979+dualsh0x@users.noreply.github.com>
2021-08-18 17:49:58 +0100

    literally everything
    its already modified a bit
    hopefully the stuff works properly for everyone else

---
+++ _maps/Rings.asm
@@ -0,0 +1,26 @@
+Map_0eb3:
+Map_0eb3_0: 	dc.w Map_0eb3_10-Map_0eb3
+Map_0eb3_2: 	dc.w Map_0eb3_16-Map_0eb3
+Map_0eb3_4: 	dc.w Map_0eb3_1C-Map_0eb3
+Map_0eb3_6: 	dc.w Map_0eb3_22-Map_0eb3
+Map_0eb3_8: 	dc.w Map_0eb3_28-Map_0eb3
+Map_0eb3_A: 	dc.w Map_0eb3_2E-Map_0eb3
+Map_0eb3_C: 	dc.w Map_0eb3_34-Map_0eb3
+Map_0eb3_E: 	dc.w Map_0eb3_3A-Map_0eb3
+Map_0eb3_10: 	dc.b $1
+	dc.b $F8, $5, $0, $0, $F8
+Map_0eb3_16: 	dc.b $1
+	dc.b $F8, $5, $0, $4, $F8
+Map_0eb3_1C: 	dc.b $1
+	dc.b $F8, $1, $0, $8, $FB
+Map_0eb3_22: 	dc.b $1
+	dc.b $F8, $5, $8, $4, $F8
+Map_0eb3_28: 	dc.b $1
+	dc.b $F8, $5, $0, $A, $F8
+Map_0eb3_2E: 	dc.b $1
+	dc.b $F8, $5, $0, $A, $F8
+Map_0eb3_34: 	dc.b $1
+	dc.b $F8, $5, $8, $A, $F9
+Map_0eb3_3A: 	dc.b $1
+	dc.b $F7, $5, $10, $A, $F8
+	even
